825e4b4daabaf7bcbd55512f871e5aa49875a119
Ali Raza <aliraza137725@gmail.com>
2023-08-22 13:34:28 +0500

    header added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <Header/>
+    
+  )
+}
+
+export default App;
